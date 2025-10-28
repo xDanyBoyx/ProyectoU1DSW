a1e2d62179daca1d24dded08e890ebd96320c7e4
Francisco Miranda <jaramira2001@gmail.com>
2025-10-28 09:29:48 -0700

    ya ahora si es la buena

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-//IMPORTAMOS EL MODELO DE USUARIOS
-const User = require("../models/user.model");
-
-//FUNCIÓN DE VALIDACIONES
-const validateUserData = (data, isNew = true) => {
-  const errors = {};
-
-  //VALIDAR NOMBRE
-  if (isNew || data.name !== undefined) {
-    if (!data.name || typeof data.name !== "string" || /\d/.test(data.name)) {
-      errors.name = "El nombre es requerido y no debe contener números.";
-    }
-  }
-
-  //ALIDAR CONTRASEÑA
-  if (isNew || data.password !== undefined) {
-    if (!data.password || data.password.length < 6) {
-      errors.password = "La contraseña debe tener al menos 6 caracteres.";
-    }
-  }
-
-  //VALIDAR CORREO
-  if (isNew || data.mail !== undefined) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!data.mail || !emailRegex.test(data.mail)) {
-      errors.mail = "El formato del correo electrónico es inválido.";
-    }
-=======
 //IMPORTAMOS EL MODELO DE USUARIOS (ESM)
 import UserModel from "../models/user.model.js";
 
@@ -45,7 +16,6 @@ const validateUserData = (data, isNew = true) => {
 
   if ((isNew || data.mail !== undefined) && (!data.mail || !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.mail))) {
     errors.mail = "El formato del correo electrónico es inválido.";
->>>>>>> conexionFirebase
   }
 
   //DEVOLVEMOS ERRORES SI EXISTEN
@@ -53,54 +23,6 @@ const validateUserData = (data, isNew = true) => {
 };
 
 //GET ALL
-<<<<<<< HEAD
-function getAll(req, res) {
-  const filters = req.query;
-
-  if (Object.keys(filters).length > 0) {
-    const data = User.filterUser(filters);
-    return res.status(200).json(data);
-  }
-
-  const data = User.findAll();
-  res.status(200).json(data);
-}
-
-//GET ID
-function getById(req, res) {
-  const id = req.params.id;
-  const user = User.findById(id);
-  if (!user) return res.status(404).json({ error: "Usuario no encontrado" });
-  res.status(200).json(user);
-}
-
-//POST
-function add(req, res) {
-  const { name, password, mail, role, domicile, rfc } = req.body;
-
-  const errors = validateUserData(req.body, true);
-  if (errors) {
-    return res.status(400).json({
-      message: "Datos de usuario inválidos",
-      errors,
-    });
-  }
-
-  const newUser = User.addUser({
-    name,
-    password,
-    mail,
-    role,
-    domicile,
-    rfc,
-  });
-
-  res.status(201).json(newUser);
-}
-
-//PUT
-function update(req, res) {
-=======
 async function getAll(req, res) {
   const filters = req.query;
 
@@ -181,7 +103,6 @@ async function add(req, res) {
 
 //PUT
 async function update(req, res) {
->>>>>>> conexionFirebase
   const id = req.params.id;
 
   const errors = validateUserData(req.body, false);
@@ -192,27 +113,6 @@ async function update(req, res) {
     });
   }
 
-<<<<<<< HEAD
-  const updatedUser = User.updateUser(id, req.body);
-  if (!updatedUser)
-    return res.status(404).json({ error: "Usuario no encontrado" });
-
-  res.status(200).json(updatedUser);
-}
-
-//DELETE
-function remove(req, res) {
-  const id = req.params.id;
-  const ok = User.deleteUser(id);
-
-  if (!ok) return res.status(404).json({ error: "Usuario no encontrado" });
-
-  res.status(200).json({ message: "Usuario eliminado correctamente" });
-}
-
-//EXPORTAMOS LAS FUNCIONES
-module.exports = {
-=======
   try {
     const updatedUser = await UserModel.updateUser(id, req.body);
     if (!updatedUser)
@@ -238,14 +138,9 @@ async function remove(req, res) {
 
 //EXPORTAMOS LAS FUNCIONES (ESM)
 export default {
->>>>>>> conexionFirebase
   getAll,
   getById,
   add,
   update,
-<<<<<<< HEAD
-  remove,
-=======
   remove
->>>>>>> conexionFirebase
 };
