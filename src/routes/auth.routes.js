23e1f866d50fcb035ae25cdbe3ae211313823f40
Francisco Miranda <150218348+FranMirandaJ@users.noreply.github.com>
2025-10-30 00:11:33 -0700

    Delete src directory

--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as controller from "../controllers/auth.controller.js";
-
-const router = express.Router();
-
-router.post("/register", controller.register);
-router.post("/login", controller.login);
-
-export default router;
