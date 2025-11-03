b6cdf552294446745ac0ec7280c4b8ab8b092915
Francisco Miranda <150218348+FranMirandaJ@users.noreply.github.com>
2025-11-02 21:41:39 -0700

    avance en front (#4)

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN_URL,
+  credentials: true
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
