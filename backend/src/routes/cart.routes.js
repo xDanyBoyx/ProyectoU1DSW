e965968ab6716e5e8d2d76b80020eb352d8ec5ce
Francisco Miranda <jaramira2001@gmail.com>
2025-12-11 02:03:01 -0700

    fix acceso de rutas por rol en carritos

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -4,16 +4,14 @@ import { authenticate, authorize } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
-router.use(authorize(['cliente'])); // Solo los usuarios con role = 'cliente' pueden operar con los carritos
-
 // RUTA SOLO PARA ADMIN, PUEDE VER TODAS LAS COMPRAS/CARRITOS ACTIVOS
 router.get('/all', authorize(['admin']), controller.getAllCarts);
 
-router.get('/', controller.getCurrentCart);
-router.post('/', controller.addProductsToCart); // agregar productos al carrito activo o crear uno nuevo si no tiene carrito
-router.post('/products/:productId', controller.updateQtyProductInCart);
-router.delete('/products/:productId', controller.removeProductFromCart);
-router.delete('/clear', controller.clearCart);
-router.post('/pay', controller.payCart);
+router.get('/', authorize(['cliente']), controller.getCurrentCart);
+router.post('/', authorize(['cliente']), controller.addProductsToCart); // agregar productos al carrito activo o crear uno nuevo si no tiene carrito
+router.post('/products/:productId', authorize(['cliente']), controller.updateQtyProductInCart);
+router.delete('/products/:productId', authorize(['cliente']), controller.removeProductFromCart);
+router.delete('/clear', authorize(['cliente']), controller.clearCart);
+router.post('/pay', authorize(['cliente']), controller.payCart);
 
 export default router;
