f34c39e2dc7def05eee2688da674753e00703a71
Francisco Miranda <jaramira2001@gmail.com>
2025-12-11 01:51:08 -0700

    endpoint para ver todos los carritos restringido a que solo el admin puede ver esa info

--- backend/src/routes/cart.routes.js
+++ backend/src/routes/cart.routes.js
@@ -6,9 +6,11 @@ const router = express.Router();
 
 router.use(authorize(['cliente'])); // Solo los usuarios con role = 'cliente' pueden operar con los carritos
 
+// RUTA SOLO PARA ADMIN, PUEDE VER TODAS LAS COMPRAS/CARRITOS ACTIVOS
+router.get('/all', authorize(['admin']), controller.getAllCarts);
+
 router.get('/', controller.getCurrentCart);
 router.post('/', controller.addProductsToCart); // agregar productos al carrito activo o crear uno nuevo si no tiene carrito
-router.get('/all', controller.getAllCarts);
 router.post('/products/:productId', controller.updateQtyProductInCart);
 router.delete('/products/:productId', controller.removeProductFromCart);
 router.delete('/clear', controller.clearCart);
